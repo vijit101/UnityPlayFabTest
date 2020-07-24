2396d70fdc657bd632e7edce837779b6a78cc084
Shubham <vijit101@gmail.com>
2020-07-24 15:41:03 +0530

    - Unity Plafab basic setup done

---
+++ Assets/Scripts/PlayFabLogin.cs
@@ -0,0 +1,37 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+public class PlayFabLogin : MonoBehaviour
+{
+    private string userEmail;
+    private string secret;
+    public void Start()
+    {
+        
+        if (string.IsNullOrEmpty(PlayFabSettings.staticSettings.TitleId))
+        {
+            /*
+            Please change the titleId below to your own titleId from PlayFab Game Manager.
+            If you have already set the value in the Editor Extensions, this can be skipped.
+            */
+            PlayFabSettings.staticSettings.TitleId = "64A28";
+        }
+        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+        //var request = new LoginWithEmailAddressRequest { Email = userEmail, Password = secret };
+        //PlayFabClientAPI.LoginWithEmailAddress(request, OnLoginSuccess, OnLoginFailure);
+    }
+
+    private void OnLoginSuccess(LoginResult result)
+    {
+        Debug.Log("Congratulations, you made your first successful API call! "+result);
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
+    }
+}
